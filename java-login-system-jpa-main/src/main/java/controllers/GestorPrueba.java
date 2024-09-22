e7e43887e23e7f83fc01e93cd281ded6e7e9c16a
GervasioMatteoda <gjmatteoda@gmail.com>
2024-09-22 19:54:35 -0300

    Octava Modificación: Funcionalidad de Suma y Resta

--- java-login-system-jpa-main/src/main/java/controllers/GestorPrueba.java
+++ java-login-system-jpa-main/src/main/java/controllers/GestorPrueba.java
@@ -14,4 +14,11 @@ public class GestorPrueba {
         }
     }
 
+    public int suma(int a, int b) {
+        return a+b;
+    }
+
+    public int resta(int a, int b) {
+        return suma(a, -b);
+    }
 }
